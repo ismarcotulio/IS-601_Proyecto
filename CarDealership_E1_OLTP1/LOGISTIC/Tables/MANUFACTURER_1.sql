cf7c2b918cfac30b569b2180b30c83f81be49d1b
Marco Ruiz <mruizq@unah.hn>
2021-10-23 16:55:54 -0600

    OLTP1 -> Actualizacion de solucion

---
+++ CarDealership_E1_OLTP1/LOGISTIC/Tables/MANUFACTURER_1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [LOGISTIC].[MANUFACTURER] (
+    [int_id_PK]       INT          IDENTITY (1, 1) NOT NULL,
+    [var_name]        VARCHAR (50) NOT NULL,
+    [var_commonName]  VARCHAR (50) DEFAULT ('') NULL,
+    [var_postalCode]  VARCHAR (50) DEFAULT ('') NULL,
+    [tin_location_FK] TINYINT      DEFAULT ((0)) NULL,
+    PRIMARY KEY CLUSTERED ([int_id_PK] ASC)
+);
+
