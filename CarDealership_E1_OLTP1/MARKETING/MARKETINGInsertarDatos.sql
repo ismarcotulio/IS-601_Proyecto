20774f66e7b0f2670254ded8f3eed7ed493a2737
jisdisai <joaquin.salgado@ymail.com>
2021-10-27 16:52:41 -0600

    agregando script Datos de Marketing

---
+++ CarDealership_E1_OLTP1/MARKETING/MARKETINGInsertarDatos.sql
@@ -0,0 +1,106 @@
+USE CAR_RENT_AND_SALE;
+SELECT*FROM MARKETING_DEPARTAMENT;
+
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (1, 'Kelly Sosa', ' area de MArketing');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (2, 'Gustavo Martinez', ' area de MArketing');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (3, 'Milton Jimenez', ' area de MArketing');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (4, 'Porfirio Sosa', ' area de MArketing');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (5, 'Adalid Rodriguez', ' area de MArketing');
+
+
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (6, 'Manuel Bolillo', ' area de Gerente de Marca');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (7, 'Rosa Aguilar', ' area de Gerente de Marca');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (8, 'Santigo Ekono', ' area de Gerente de Marca');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (9, 'Josue Avila', ' area de Gerente de Marca');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (10, 'Nastacha Orvillo', ' area de Gerente de Marca');
+
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (11, 'Neptali Maradiaga', ' area de Campaña Publicitaria');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (12, 'Manule Sosa', ' area de Campaña Publicitaria');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (13, 'Cristian Palma', ' area de Campaña Publicitaria');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (14, 'Gerson Rodriguez', ' area de Campaña Publicitaria');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (15, 'jose nuñez', ' area de Campaña Publicitaria');
+
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (16, 'Mateo Yibrin', ' area de Estudio de mercado');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (17, 'Ezequiel Medina', ' area de Estudio de mercado');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (18, 'Rosa Palacios', ' area de Estudio de mercado');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (19, 'Natalia Mora', ' area de Estudio de mercado');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (20, 'Esmeralda Matews', ' area de Estudio de mercado');
+
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (21, 'Jose Carrasco', ' area de Redes sociales');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (22, 'Jose Mendoza', ' area de Redes sociales');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (23, 'Rafael Galdamez', ' area de Redes sociales');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (24, 'Vertulio Madina', ' area de Redes sociales');
+INSERT INTO MARKETING_DEPARTAMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (25, 'Jose Medina', ' area de Redes sociales');
+
+SELECT*FROM MARKETING_BRAND_MANAGEMENT;
+INSERT INTO MARKETING_BRAND_MANAGEMENT(tin_id_PK, var_name, tex_description) 
+    VALUES (1, 'Jose Medina', 'realizar los cambios de marcas ');
+INSERT INTO MARKETING_BRAND_MANAGEMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (2, 'Rosa Aguilar', 'realizar los cambios de marcas ');
+INSERT INTO MARKETING_BRAND_MANAGEMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (3, 'Santiago Ekono', ' Supervisar los cambios realizados');
+INSERT INTO MARKETING_BRAND_MANAGEMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (4, 'Josue Avila', 'tomar visto bueno ');
+INSERT INTO MARKETING_BRAND_MANAGEMENT (tin_id_PK, var_name, tex_description) 
+    VALUES (5, 'Nastacha Orvillo', 'Derechos de Marca ');
+
+	SELECT*FROM MARKETING_CAMPAIGN_ADVERTISING;
+INSERT INTO MARKETING_CAMPAIGN_ADVERTISING (tin_id_PK, var_name, tex_description) 
+    VALUES (1, 'Neptali Maradiaga', 'Campaña publicitaria en Television ');
+INSERT INTO MARKETING_CAMPAIGN_ADVERTISING (tin_id_PK, var_name, tex_description) 
+    VALUES (2, 'Manule Sosa', 'Campaña publicitaria en Televison ');
+INSERT INTO MARKETING_CAMPAIGN_ADVERTISING (tin_id_PK, var_name, tex_description) 
+    VALUES (3, 'Cristian Palma', 'Campaña publicitaria en Radio ');
+INSERT INTO MARKETING_CAMPAIGN_ADVERTISING (tin_id_PK, var_name, tex_description) 
+    VALUES (4, 'Gerson Rodriguez', 'Campaña publicitaria en Pancartas');
+INSERT INTO MARKETING_CAMPAIGN_ADVERTISING (tin_id_PK, var_name, tex_description) 
+    VALUES (5, 'jose nuñez', 'Campaña publicitaria en la tienda');
+
+	SELECT*FROM MARKETING_MARKET_STUDY;
+INSERT INTO MARKETING_MARKET_STUDY (tin_id_PK, var_name, tex_description) 
+    VALUES (1, 'Mateo Yibrin', 'Estudio del Mercado altas y bajas');
+INSERT INTO MARKETING_MARKET_STUDY (tin_id_PK, var_name, tex_description) 
+    VALUES (2, 'Ezequiel Medina', 'Estudio del Mercado altas y bajas');
+INSERT INTO MARKETING_MARKET_STUDY (tin_id_PK, var_name, tex_description) 
+    VALUES (3, 'Rosa Palacios', 'Estudio de la marca que tan impato hay');
+INSERT INTO MARKETING_MARKET_STUDY (tin_id_PK, var_name, tex_description) 
+    VALUES (4, 'Natalia Mora', 'Estudio de las promociones');
+INSERT INTO MARKETING_MARKET_STUDY (tin_id_PK, var_name, tex_description) 
+    VALUES (5, 'Esmeralda Matews', 'Estudio de las persecion del cliente');
+
+	SELECT*FROM MARKETING_SOCIAL_NETWORKS;
+INSERT INTO MARKETING_SOCIAL_NETWORKS(tin_id_PK, var_name, tex_description) 
+    VALUES (1, 'Jose Carrasco', 'Publicidad en Facebook');
+INSERT INTO MARKETING_SOCIAL_NETWORKS(tin_id_PK, var_name, tex_description) 
+    VALUES (2, 'Jose Mendoza', 'Publicidad en WhatsApp');
+INSERT INTO MARKETING_SOCIAL_NETWORKS(tin_id_PK, var_name, tex_description) 
+    VALUES (3, 'Rafael Galdamez', 'Publicidad en Instagrams');
+INSERT INTO MARKETING_SOCIAL_NETWORKS(tin_id_PK, var_name, tex_description) 
+    VALUES (4, 'Vertulio Madina', 'Publicidad en Twitter');
+INSERT INTO MARKETING_SOCIAL_NETWORKS(tin_id_PK, var_name, tex_description) 
+    VALUES (5, 'Jose Medina', 'Publicidad en Tik Tok');
