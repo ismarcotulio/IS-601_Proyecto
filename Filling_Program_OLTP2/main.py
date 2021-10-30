03e89c1f2627bb5f6f9a19070d166e5c3237123e
Marco Ruiz <mruizq@unah.hn>
2021-10-30 00:39:49 -0600

    OLTP2 -> Historico de ventas por cada vehiculo
    1. Se agrego un algoritmo que permite extraer el histórico de ventas de un vehículo en especifico.
    2-Se optimizo el rendimiento en la busqueda de VINs validos.
    3. Se agrego la version final del json que contiene la lista de VINs.

--- Filling_Program_OLTP2/main.py
+++ Filling_Program_OLTP2/main.py
@@ -6,32 +6,35 @@
 """
 
 from Classes.MongoDB import MongoDB
-from Classes.JSONHandler import JSONHandler
+from Classes.DataHandler import DataHandler
 from Classes.CarMDHandler import CarMDHandler
+from Classes.DataExtractor import DataExtractor
 
 def main():    
     
     mongo = MongoDB()
     carMD = CarMDHandler()
-    jsonHandler = JSONHandler(mongo, carMD)
+    dataExtractor = DataExtractor()
+    dataHandler = DataHandler(mongo, carMD, dataExtractor)
+    
     limit = 3
-    count = 0
+    count = 0   
 
-
+    
     while count < limit:
-        vehicleData = jsonHandler.buildVehicleData(
-            jsonHandler.getLastValidPost(),
-            jsonHandler.getLastValidAccount()
+        postDataList = dataHandler.getLastValidPost()
+        tables = postDataList[2]
+
+        vehicleData = dataHandler.buildVehicleData(
+            postDataList[0],
+            postDataList[1],
+            dataHandler.getLastValidAccount(),
+            dataExtractor.getJsonFromTable(tables[3])
         )
 
         mongo.insertToCollection(vehicleData)
 
         count = count + 1
-
-    
-    
-   
-
 
 
 
