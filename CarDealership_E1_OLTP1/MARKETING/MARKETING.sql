59bc4edae7c63376ad30b9bb7d26822af0c9d82c
jisdisai <joaquin.salgado@ymail.com>
2021-11-04 19:25:21 -0600

    agregando agregando en marketing sale promotion y marketing promotion

--- CarDealership_E1_OLTP1/MARKETING/MARKETING.sql
+++ CarDealership_E1_OLTP1/MARKETING/MARKETING.sql
@@ -37,6 +37,22 @@ USE CAR_RENT_AND_SALE;
  tin_departament_FK TINYINT , FOREIGN KEY (tin_departament_FK) REFERENCES  MARKETING_DEPARTAMENT(tin_id_PK)
  );
 
+CREATE TABLE [MARKETING.PROMOTION] (
+    [tin_area_id_PK] TINYINT      IDENTITY (1, 1) NOT NULL,
+    [tex_description]       VARCHAR (60) NOT NULL,
+	[tex_type]       VARCHAR (60) NOT NULL,
+    PRIMARY KEY CLUSTERED ([tin_area_id_PK] ASC)
+);
+
+CREATE TABLE [MARKETING.SALE_PROMOTION] (
+    [tin_area_id_PK] TINYINT      IDENTITY (1, 1) NOT NULL,
+    [tex_description]       VARCHAR (60) NOT NULL,
+	[date_start_date]       DATETIME,
+	[date_start_final]		DATETIME,
+    PRIMARY KEY CLUSTERED ([tin_area_id_PK] ASC)
+);
+
+
 
 
 
