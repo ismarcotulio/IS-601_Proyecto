ede65a993edf6dbd5c1ab2565149acbea1700119
Marco Ruiz <mruizq@unah.hn>
2021-11-19 17:18:01 -0600

    OLTP1 -> Docker en rama master

--- Filling_Docker_OLTP1/src/myStartupScript.sh
+++ Filling_Docker_OLTP1/src/myStartupScript.sh
@@ -7,8 +7,6 @@
     cd /opt/mssql-tools/bin && ./sqlcmd -S db -U SA -P "BD2_Grupo1" -Q "CREATE DATABASE CarDealership_OLTP1"
     echo "BASE DE DATOS CREADA"
     cd /home/resources/IS-601_Proyecto
-    echo "Cambiando a rama de prueba"
-    git checkout TestBranch
     git pull
     cd Filling_Program_OLTP1/FAKERnodeJS
     echo "preparando el programa en node"
@@ -17,10 +15,12 @@
     echo "---------------------------------------------------------------------------"
     echo "--------------EJECUTANDO PROGRAMA DE LLENADO EN NODEJS----------------------"
     echo "---------------------------------------------------------------------------"
+    echo "Por favor espere, esto puede tomar horas debido al gran volumen de datos"
     npm run dev
     echo "---------------------------------------------------------------------------"
     echo "--------------EJECUTANDO PROGRAMA DE LLENADO EN LARAVEL--------------------"
     echo "---------------------------------------------------------------------------"
+    echo "Por favor espere, esto puede tomar horas debido al gran volumen de datos"
     cd /home/resources/IS-601_Proyecto/Filling_Program_OLTP1
     php artisan migrate --seed
     echo "Gracias por la espera, base de datos llenada correctamente."
